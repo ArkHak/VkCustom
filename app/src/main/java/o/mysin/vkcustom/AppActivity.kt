497390ec2368068d6ca53b569fff926c1f5acddd
Oleg Mysin <7nacu7@gmail.com>
2024-08-22 23:41:07 +0300

    Test lesson -> 2.3

--- app/src/main/java/o/mysin/vkcustom/AppActivity.kt
+++ app/src/main/java/o/mysin/vkcustom/AppActivity.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import o.mysin.vkcustom.ui.theme.VkCustomTheme
 
 class AppActivity : ComponentActivity() {
@@ -23,8 +30,12 @@ class AppActivity : ComponentActivity() {
         setContent {
             VkCustomTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)) {
-                        UserInfo(name = "Олег", age = 5)
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        TimesTable()
                     }
                 }
             }
@@ -32,25 +43,40 @@ class AppActivity : ComponentActivity() {
     }
 }
 
-
-@Composable
-fun UserInfo(
-    name: String,
-    age: Int,
-) {
-    Text(
-        text = "Привет $name, тебе $age лет",
-        color = Color.Green
-    )
-}
-
 @Preview
 @Composable
-fun UserInfoPreview() {
-    Column {
-        repeat(10) {
-            UserInfo(name = "Олег", age = 8)
-            UserInfo(name = "Юля", age = 6)
+fun TimesTable() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        for (i in 1..9) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            ) {
+                for (j in 1..9) {
+                    val value = i * j
+                    val color = if ((i + j) % 2 == 0) {
+                        Color.Yellow
+                    } else {
+                        Color.White
+                    }
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f)
+                            .background(
+                                color = color
+                            )
+                            .border(width = 1.dp, color = Color.DarkGray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text(text = "$value")
+                    }
+                }
+            }
         }
     }
 }
